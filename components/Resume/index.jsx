32a42276e80914522208e608415fad3325dadce1
rinava <asdasd5056@gmail.com>
2022-07-25 13:09:01 -0300

    fix resume aspect ratio

--- components/Resume/index.jsx
+++ components/Resume/index.jsx
@@ -2,15 +2,22 @@ import styles from './styles.module.scss';
 
 const Resume = () => {
   return (
-    <div className={styles.resume}>
-      <iframe
-        src='https://drive.google.com/file/d/1a3UdKnpOoD7jo2O8mlnDekLThygyUeVN/preview?usp=sharing#view=fitH'
-        title='Lara Mateo Resume'
-        type='application/pdf'
-        // frameBorder='0'
-        // allowFullScreen
-      />
-    </div>
+    <>
+      <div className={styles.resume}>
+        <iframe
+          src='https://drive.google.com/file/d/1a3UdKnpOoD7jo2O8mlnDekLThygyUeVN/preview?usp=sharing#view=fitW'
+          title='Lara Mateo Resume'
+          type='application/pdf'
+          frameBorder='0'
+        />
+      </div>
+      <a
+        href='/assets/Lara-Mateo-Resume.pdf'
+        download='Lara Mateo Resume'
+        className={styles.download}>
+        Download Resume
+      </a>
+    </>
   );
 };
 
